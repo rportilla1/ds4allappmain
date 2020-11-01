43b6c1cdfc92555108b84fe903022d68a9e75897
rportilla1 <43485406+rportilla1@users.noreply.github.com>
2020-10-31 23:17:34 -0400

    Version 3 Oct31

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import dash
-from dash.dependencies import Input, Output, State
+#from dash.dependencies import Input, Output, State
 from datetime import date
 import dash_html_components as html
 import dash_core_components as dcc
@@ -7,9 +7,9 @@ import dash_bootstrap_components as dbc
 import pandas as pd
 import dash_table
 import plotly.express as px
-from sqlalchemy import create_engine
+#from sqlalchemy import create_engine
 import psycopg2 as ps
-import pandas as pd 
+#import pandas as pd
 #import pyodbc
 
 
@@ -22,39 +22,8 @@ colors = {
     'color': '#333333'
 }
 
-# the style arguments for the sidebar.
-# the style arguments for the sidebar.
-SIDEBAR_STYLE = {
-    'position': 'fixed',
-    'top': 0,
-    'left': 0,
-    'bottom': 0,
-    'width': '20%',
-    'padding': '20px 10px',
-    'background-color': '#1E90FF'
-}
 
-# the style arguments for the main content page.
-CONTENT_STYLE = {
-    'margin-left': '25%',
-    'margin-right': '5%',
-    'padding': '20px 10p'
-}
-
-TEXT_STYLE = {
-    'textAlign': 'center',
-    'color': '#FFFFFF'
-}
-
-CARD_TEXT_STYLE = {
-    'textAlign': 'justify',
-    'color': '#000000'
-    'width: 120rem',
-}
-
- 
-
-
+# conection to DB
 host = '157.230.55.87'
 port = 5432
 user = 'postgres'
@@ -69,67 +38,56 @@ else:
     print('Connected!')
 
 SQL_Query = pd.read_sql('SELECT * FROM eventosmodelo LIMIT 100', conn)
+# charts
 scatter = px.scatter(SQL_Query, x='longitud', y='latitud')
 
 #### ------------- application
 
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+app = dash.Dash(__name__
+, external_stylesheets=[dbc.themes.BOOTSTRAP]
+)
 server = app.server
 
 # CONTROLS:
 
-     
  # define visual objects and styles
 sidebar = html.Div(
-    [   
-        html.H2('FILTROS AVANZADOS', style=TEXT_STYLE),
-        html.Hr(),
+    [
+        html.H5('FILTROS AVANZADOS'),
         #controls,
-        dcc.DatePickerRange(id='date_picker', min_date_allowed=date(2019,11,1),
+        dcc.DatePickerRange(id='date_picker1', min_date_allowed=date(2019,11,1),
                     max_date_allowed=date(2019,11,30),initial_visible_month =date(2019,11,1),
                     minimum_nights = 0),
-dcc.Input(id='input_hour',type='text', placeholder='Start hour', debounce=True,
+        dcc.Input(id='input_hour1',type='text', placeholder='Start hour', debounce=True,
           pattern= u'^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$'),
-dcc.Input(id='output_hour',type='text', placeholder='End hour', debounce=True,
+        dcc.Input(id='output_hour1',type='text', placeholder='End hour', debounce=True,
           pattern= u'^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$'),
-html.Button(id= 'date_button', n_clicks=0, children = 'Process')
+        html.Button(id= 'date_button1', n_clicks=0, children = 'Process')
     ],
-    style=SIDEBAR_STYLE,
 )
 
 # banner object
-banner = html.Div(
-            className="banner",
-            children=[
-                # Change App Name here
-                html.Div(
-                    className="container scalable",
-                    children=[
+nav = dbc.Navbar(
+         [
+            html.A(
+                # Use row and col to control vertical alignment of logo / brand
+                dbc.Row(
+                    [
                         # Change App Name here
-                        html.H2(
-                                                                               
-                            id="banner-title",
-                            children=[
-                                html.A(
-                                    "DASHBOARD AMVA",
-                                     style={
-                                        "text-decoration": "none",
-                                        "color": "#FFFFFF",
-                                        
-                                    },
-                                )
-                            ],
-                        ),
-                        html.A(
-                            id="banner-logo",
-                            children=[
-                                html.Img(src=app.get_asset_url("logo5.png"))
-                            ],
-                         ),
+                        dbc.Col(html.Img(src=app.get_asset_url('logo5.png'), height="120px")),
+                        dbc.Col(dbc.NavbarBrand("DASHBOARD AMVA", className = "ml-2" )),
                     ],
-                )
-            ],
-        )
+                    align="center",
+                    no_gutters=True,
+            ),
+            href="https://plot.ly",
+        ),
+#        dbc.NavbarToggler(id="navbar-toggler"),
+#        dbc.Collapse(search_bar, id="navbar-collapse", navbar=True),
+    ],
+    color="dark",
+    dark=True,
+)
 
 # intro div
 intro = html.Div(
@@ -138,7 +96,7 @@ intro = html.Div(
                                 [
                                     html.H5("INTRODUCCIÓN", style={
                                         "color": "#15283F"}),
-                                    
+
                                     html.P(
                                         "\
                                     El transporte público es un servicio vital para una ciudad, ya que permite a sus ciudadanos desplazarse,  viajar y desarrollar la economía de la ciudad. Además, se ha trabajado mucho en el diseño de un sistema  de transporte inclusivo, eficiente y sostenible. Sin embargo, ha habido una falta de coordinación de las  diferentes partes interesadas y hay una falta de datos confiables para los tomadores de decisiones. Las  partes interesadas del sistema de transporte pueden incluir: gobierno, autoridades de tránsito, empresas    propietarias de autobuses, formuladores de políticas, fabricantes de autobuses, conductores, usuarios,  etc. Pueden tener diferentes intereses y percepciones sobre cómo debería ser un sistema de transporte                                       público y una metodología sistemática para la evaluación de los principales factores de servicio no está                                     disponible, más aún cuando los datos están disponibles, pero son diversos, dispersos y no confiables. El                                     Centro de Operaciones de Transporte Público (GTPC) reconoce que hasta el 40% de los datos capturados                                         para el sistema operativo diario tienen problemas de calidad. Los datos de baja calidad no son adecuados                                     para alimentar el sistema operativo ni utilizarse para el modelado de predicciones, la planificación de                                     rutas óptimas o para la toma de decisiones.",
@@ -151,7 +109,58 @@ intro = html.Div(
                         ],
                         className="row",
                     )
+#id="imagen-intro",
+imagen = html.Div([html.A(children=[
+                                html.Img(src=app.get_asset_url("banner.jpg"),
+                                         style={
+                'height': '90%',
+                'width': '90%'
+            })
+                            ],
+                         )
+], style={'textAlign': 'center'})
 
+#OUR TEAM
+# intro div
+NOSOTROS = html.Div(
+                        [
+                            html.Div(
+                                [
+                                    html.H5("NUESTRO EQUIPO", style={
+                                        "color": "#15283F"}),
+
+                                    html.P(
+                                        "\
+                                    Contamos con el mejor talento de Científicos de Datos certificados con Honores por el Ministerio de Tecnologías de la Información y Comunicaciones (MINTIC). Somos Expertos en la generación de soluciones de Negocio, aplicamos alternativas basadas en Inteligencia Artificial, expertos en bases de datos, integración de herramientas y metodologías tecnologicas para un mejor desarrollo y sostenibilidad del país. Somos tu mejor Opción :)" ,
+                                        style={"color": "#15283f"},
+                                        className="row",
+                                    ),
+                                ],
+                                className="product",
+                            )
+                        ],
+                        className="row",
+                    )
+#id="imagen-intro",
+EQUITEAM = html.Div([html.A(children=[
+                                html.Img(src=app.get_asset_url("TEAM.png"),
+                                         style={
+                'height': '100%',
+                'width': '100%'
+            })
+                            ],
+                         )
+], style={'textAlign': 'center'})
+
+
+
+
+
+
+
+
+
+# GRAPHS
 graph1 = dcc.Graph(figure=scatter, id='scatter')
 graph2 = dash_table.DataTable(id='table',
                      columns =[{'name':i, 'id':i} for i in SQL_Query.columns],
@@ -174,24 +183,24 @@ graph3 = dcc.Graph(
         }
     )
 
-content_first_row = dbc.Row(dbc.Col(banner,md=12))
+content_first_row = dbc.Row(dbc.Col(''))
 
 content_second_row = dbc.Row(dbc.Col(intro,md=12))
 
 content_third_row = dbc.Row([
+
     dbc.Col(
-        dbc.Card(
+    dbc.Card(
             [
                 dbc.CardBody(
                     [
-                        html.H4(id='TITL', children=['# RUTAS'], className='card-title',
-                                style='width: 22rem;'),
-                        html.P(id='card_text_1', children=[str(SQL_Query['latitud'].mean())], style=CARD_TEXT_STYLE),
+                        html.H4(id='TITL', children=['# RUTAS'], className='card-title'),
+                        html.P(id='card_text_1', children=[str(SQL_Query['latitud'].mean())]),
                     ]
                 )
             ], color="primary", inverse=True
         ),
-        md=3
+        md=2
     ),
     dbc.Col(
         dbc.Card(
@@ -199,46 +208,47 @@ content_third_row = dbc.Row([
 
                 dbc.CardBody(
                     [
-                        html.H4('# EMPRESAS', className='card-title', style='width: 22rem;'),
-                        html.P('Sample text.', style=CARD_TEXT_STYLE),
+                        html.H4('# EMPRESAS', className='card-title'),
+                        html.P('Sample text.'),
                     ]
                 )
             ] ,color="primary", inverse=True
 
         ),
-        md=3
+        md=2
     ),
     dbc.Col(
         dbc.Card(
             [
                 dbc.CardBody(
                     [
-                        html.H4('# BACH', className='card-title', style='width: 22rem;'),
-                        html.P('Sample text.', style=CARD_TEXT_STYLE),
+                        html.H4('# BACH', className='card-title'),
+                        html.P('Sample text.'),
                     ]
                 )
             ] ,    color="primary", inverse=True
 
         ),
-        md=3
+        md=2
     ),
     dbc.Col(
         dbc.Card(
             [
                 dbc.CardBody(
                     [
-                        html.H4('# LINEA', className='card-title', style='width: 22rem;'),
-                        html.P('Sample text.', style=CARD_TEXT_STYLE, ),
+                        html.H4('# LINEA', className='card-title'),
+                        html.P('Sample text.'),
                     ]
                 )
             ],    color="primary", inverse=True
         ),
-        md=3
+        md=2
     )
 ])
 
 content_fourth_row = dbc.Row(
     [
+
         dbc.Col(
             dcc.Graph(id='graph_1'), md=4
         ),
@@ -270,26 +280,43 @@ content_sixth_row = dbc.Row(
     ]
 )
 
+tabs = dbc.Tabs(
+    [
+        dbc.Tab([content_second_row, imagen], label="QUIENES SOMOS"),
+        dbc.Tab([NOSOTROS, EQUITEAM], label="NUESTRO EQUIPO"),
+        dbc.Tab([html.Hr(),
+                 sidebar,
+                 html.Hr(),
+                 content_third_row,
+                 html.Hr(),
+                 content_fifth_row,
+                 html.Hr(),
+                 content_sixth_row,
+                 html.Hr(),
+                 content_fourth_row], label="EDA")
+    ]
+)
+
 content = html.Div(
     [
         #html.H2('Analytics Dashboard', style=TEXT_STYLE),
         #html.Hr(),
-        html.Div(content_first_row),
-        html.Div(content_second_row),
-        html.Hr(className="myHr"),
-        html.Div(content_third_row),
+        html.Div(content_first_row), # BANNER
         html.Hr(),
-        html.Div(content_fifth_row),
-        html.Hr(),
-        html.Div(content_sixth_row),
-        html.Hr(),
-        html.Div(content_fourth_row)
+        html.Div(tabs),
+        #html.Hr(className="myHr"),
+        #html.Div(content_third_row),
+        #html.Hr(),
+        #html.Div(content_fifth_row),
+        #html.Hr(),
+        #html.Div(content_sixth_row),
+        #html.Hr(),
+        #html.Div(content_fourth_row)
     ],
-    style=CONTENT_STYLE
 )
 
 #### layout
-app.layout = html.Div(children=[sidebar,
+app.layout = html.Div(children=[nav,
                                 content,
 
 
@@ -297,23 +324,23 @@ app.layout = html.Div(children=[sidebar,
 html.H5("EXPLORACIÓN DE DATOS"),
 
 #graph1,
-    
+
 
 #html.Br([]),
-#html.H5("RUTAS REPORTADAS"),      
+#html.H5("RUTAS REPORTADAS"),
 
 #graph2,
 
 #html.Br([]),
 
 #html.H5("MAPEO"),
-    
-#graph3,   
+
+#graph3,
 
 html.Div(id='output_date')
 ], id='layout')
 
-  
+
 
 #### Interactividad
 
